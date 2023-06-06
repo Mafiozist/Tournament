00f23280f31f99e60a90115a9404976485a18815
dinar <chaneldinara@gmail.com>
2023-06-06 13:38:13 +0700

    Added insert feature into editDialog

--- ClientApp/src/components/participants/EditDialog.js
+++ ClientApp/src/components/participants/EditDialog.js
@@ -1,29 +1,21 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
-import { DatePicker } from '@mui/x-date-pickers/DatePicker';
-import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
-import { Checkbox } from 'antd';
-import FormGroup from '@mui/material/FormGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { MenuItem, Select } from '@mui/material';
+import {  Slide, Zoom } from '@mui/material';
 import { useParticipants } from '../../redux/hooks/useParticipants';
 import { useActions } from '../../redux/hooks/useActions';
 import cloneDeep from 'lodash/cloneDeep';
-import { loadServerRows } from '@mui/x-data-grid-generator';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 export default function EditDialog(props) {
 
     const {members} = useParticipants(props.id);
-    const {updateParticipant} = useActions();
+    const {updateParticipant, addToParticipants} = useActions();
 
     let localMember = cloneDeep(members);
     const [member, setMember] = React.useState({...localMember});
@@ -36,6 +28,15 @@ export default function EditDialog(props) {
         props.close({open: false});
     };
 
+    const handleReopen = () => {
+        
+        addToParticipants({...member, id: localMember.sort( (a,b) => a.id - b.id).at(localMember.length - 1).id += 1});      
+        props.close({open: false});
+        setTimeout(() => {
+            props.close({open: true});
+        }, 350);
+    }
+
     const handleSave = () => {
         updateParticipant({...member});
         props.close({open: false});
@@ -45,86 +46,92 @@ export default function EditDialog(props) {
 
     return (
         <div>
-            <Dialog open={props.visible} onClose={handleClose}>
-                <DialogTitle>Редактирование пользователя </DialogTitle>
-                <DialogContent style={{display:'grid', width: 'fit-content', margin:'auto'}}>
-                    <DialogContentText hidden={deleted} style={{gridColumn: 1, gridRow: 1, textAlign:'center', backgroundColor:"#ffcccb"}}>
-                        Пользователь удален
-                    </DialogContentText>
+            
+                <Dialog TransitionComponent={ props.addState? Slide : Zoom } TransitionProps={props.visible? {direction:'right'} : {direction:'left'}} open={props.visible} onClose={handleClose}>
+                    <DialogTitle>
+                            {   
+                                props.addState?
+                                    'Добавление пользователя'
+                                :
+                                    'Редактирование пользователя'
+                            }
+                    </DialogTitle>
+                    <DialogContent style={{display:'grid', width: 'fit-content', margin:'auto'}}>
+                        <DialogContentText hidden={deleted} style={{gridColumn: 1, gridRow: 1, textAlign:'center', backgroundColor:"#ffcccb"}}>
+                            Пользователь удален
+                        </DialogContentText>
 
-                    <TextField
-                        margin="dense"
-                        id="second_name"
-                        label="Фамилия"
-                        type="text"
-                        variant="outlined"
-                        value={member.second_name}
-                        onChange={e => {
-                            setMember({...member, second_name: e.target.value});
-                        }}
-                        style={{gridColumn: 1, gridRow: 2}}
-                    />
+                        <TextField
+                            margin="dense"
+                            id="second_name"
+                            label="Фамилия"
+                            type="text"
+                            variant="outlined"
+                            value={member.second_name}
+                            onChange={e => {
+                                setMember({...member, second_name: e.target.value});
+                            }}
+                            style={{gridColumn: 1, gridRow: 2}}
+                        />
 
-                    <TextField
+                        <TextField
 
-                        margin="dense"
-                        id="first_name"
-                        label="Имя"
-                        type="text"
-                        variant="outlined"
-                        value={member.first_name}
-                        onChange={e => {
-                            setMember({...member, first_name: e.target.value});
-                        }}
-                        style={{gridColumn: 1, gridRow: 3}}
-                    />
+                            margin="dense"
+                            id="first_name"
+                            label="Имя"
+                            type="text"
+                            variant="outlined"
+                            value={member.first_name}
+                            onChange={e => {
+                                setMember({...member, first_name: e.target.value});
+                            }}
+                            style={{gridColumn: 1, gridRow: 3}}
+                        />
 
-                    <TextField
-       
-                        margin="dense"
-                        id="patronymic"
-                        label="Отчество"
-                        type="text"
-                        variant="outlined"
-                        value={member.patronymic}
-                        onChange={e => {
-                            setMember({...member, patronymic: e.target.value})
-                        }}
-                        style={{gridColumn: 1, gridRow: 4}}
-                    />
+                        <TextField
+        
+                            margin="dense"
+                            id="patronymic"
+                            label="Отчество"
+                            type="text"
+                            variant="outlined"
+                            value={member.patronymic}
+                            onChange={e => {
+                                setMember({...member, patronymic: e.target.value})
+                            }}
+                            style={{gridColumn: 1, gridRow: 4}}
+                        />
 
-                    <TextField
-                
-                        margin="dense"
-                        id="depart"
-                        label="Отдел(цех)"
-                        type="text"
-                        variant="outlined"
-                        value={member.depart}
-                        onChange={e =>{ 
-                            setMember({...member, depart: e.target.value})
-                        }}
-                        style={{gridColumn: 1, gridRow: 5}}
-                    />
+                        <TextField
+                    
+                            margin="dense"
+                            id="depart"
+                            label="Отдел(цех)"
+                            type="text"
+                            variant="outlined"
+                            value={member.depart}
+                            onChange={e =>{ 
+                                setMember({...member, depart: e.target.value})
+                            }}
+                            style={{gridColumn: 1, gridRow: 5}}
+                        />
 
-                    {/* <Select id='gender'>
-                        <MenuItem  value={'М'}>М</MenuItem>
-                        <MenuItem  value={'Ж'}>Ж</MenuItem>
-                    </Select> */}
+                    </DialogContent>
+                    <DialogActions>
+                        {props.addState?
+                                <>
+                                    <Button onClick={handleClose}>Сохранить и выйти</Button>
+                                    <Button onClick={handleReopen}>Еще <ArrowForwardIosIcon/></Button>
+                                </>
+                            :
+                                <>
+                                    <Button hidden={deleted} onClick={handleClose}  style={{justifyContent:'left'}}>Востановить</Button>
+                                    <Button onClick={handleSave}  style={{justifyContent:'right'}}>Сохранить</Button>
+                                </>
+                        }
+                    </DialogActions>
+                </Dialog>
 
-                    {/* <LocalizationProvider dateAdapter={AdapterDayjs}>
-                        <DemoContainer components={['DatePicker']}>
-                            <DatePicker label="Дата рождения" />
-                        </DemoContainer>
-                    </LocalizationProvider> */}
-
-
-                </DialogContent>
-                <DialogActions>
-                    <Button hidden={deleted} onClick={handleClose}  style={{justifyContent:'left'}}>Востановить</Button>
-                    <Button onClick={handleSave}  style={{justifyContent:'right'}}>Сохранить</Button>
-                </DialogActions>
-            </Dialog>
         </div>
     );
 }
--- ClientApp/src/components/participants/Participants.js
+++ ClientApp/src/components/participants/Participants.js
@@ -1,9 +1,10 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Component } from "react";
-import {InputLabel, TextField } from '@mui/material';
+import {IconButton, InputLabel, Popover, TextField } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
-
+import Stack from '@mui/material/Stack';
 import { SpeedDial, SpeedDialAction, Backdrop, SpeedDialIcon } from "@mui/material";
+import {LoadingButton} from "@mui/lab"
 
 //icons
 import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
@@ -11,13 +12,17 @@ import SaveIcon from '@mui/icons-material/Save';
 import PrintIcon from '@mui/icons-material/Print';
 import ShareIcon from '@mui/icons-material/Share';
 import AddIcon from '@mui/icons-material/Add'
-
+import DeleteIcon from '@mui/icons-material/Delete';
+import UndoIcon from '@mui/icons-material/Undo';
+import Tooltip from '@mui/material/Tooltip';
 import { Select,FormControl,MenuItem } from "@mui/base";
 import { GridApi ,useGridApiContext,useGridApiRef } from '@mui/x-data-grid';
 import EditDialog from "./EditDialog";
 import { useParticipants } from "../../redux/hooks/useParticipants";
 import { useActions } from "../../redux/hooks/useActions";
 import { Button } from "reactstrap";
+import { event } from "jquery";
+import { green, red } from "@mui/material/colors";
 
 //This component is worked for adding new users and represent them somehow
 
@@ -30,18 +35,23 @@ const actions = [
 ];
 
 export function Participants() {
-
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading]= useState(false);
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
+    const handleOpenAddDialog = () =>{
+        setAddState(true);
+        handleClose();
+        setShowEditDialog({open: true, id: undefined});
+    }
     const apiRef = useGridApiRef();
+    const [addState, setAddState] = useState(false);
 
     //edit-form
     const [showEditDialog, setShowEditDialog] = useState({open: false, id: 0});
 
     const {members} = useParticipants();
-    const { addToParticipants, removeFromParticipants} = useActions();
+    const { addToParticipants, setRemoveFromParticipants, setRowLoading} = useActions();
 
     //const [age, setAge] = React.useState(12);
 
@@ -50,6 +60,7 @@ export function Participants() {
         second_name: false,
         first_name: false,
         id:false,
+        is_deleted:false,
     }); 
 
     const columns = [
@@ -63,59 +74,101 @@ export function Participants() {
             description: 'This column has a value getter and is not sortable.',
             sortable: false,
             width: 160,
+            flex: 1,
             valueGetter: (params) =>
               `${params.row.second_name || ''} ${params.row.first_name || ''} ${params.row.patronymic || ''}` ,
         },
-        { field: 'age', headerName: 'Возраст' },
+        { field: 'age', headerName: 'Возраст',flex: 1, },
         { field: 'gender', headerName: 'Пол'},
-        { field: 'role', headerName: 'Роль'},
-        { field: 'depart', headerName: 'Отдел/Цех'},
+        { field: 'role', headerName: 'Роль',flex: 1,},
+        { field: 'depart', headerName: 'Отдел/Цех',flex: 1,},
         {
           field: 'is_deleted',
           headerName: 'Удален',
           type: 'boolean',
           //renderCell: (str)=> str;
         },
-        
+        {
+            field: 'action',
+            headerName: '',
+            width: 180,
+            sortable: false,
+            //disableClickEventBubbling: true,
+
+            renderCell: (params) => {
+
+                const remove = (e) => {
+                  setRowLoading({row: params.row, loading: true});
+                  const currentRow = params.row;
+                  
+                  setTimeout(() => {
+                    setRemoveFromParticipants({row: currentRow, is_deleted: true});
+                  }, 400);
+                };
+                
+                const back = (e) => {
+                    setRowLoading({row: params.row, loading: true});
+                    const currentRow = params.row;
+                    
+                    setTimeout(() => {
+                        setRemoveFromParticipants({row: currentRow, is_deleted: false});
+                    }, 400);
+                };
+
+                return (
+                    
+                    params.row.is_deleted?
+
+                            <Tooltip title="Вернуть" placement="right">
+                                <LoadingButton loading={params.row.loading}  variant="text" loadingPosition="center" onClick={back}><UndoIcon/></LoadingButton>
+                            </Tooltip>
+                            :
+                            <Tooltip title="Удалить" placement="right"> 
+                                <LoadingButton loading={params.row.loading} variant="text" color="error" loadingPosition="center" onClick={remove}><DeleteIcon/></LoadingButton >
+                            </Tooltip>
+                    
+                );
+            },
+        },
       ];
       
       const rows = [
-        { id: 1, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович', age: 33, gender: 'М', role: 'участник',depart: 1, is_deleted: false },
-        { id: 2, first_name: 'SnowSnow', second_name: 'Jon', patronymic:'Олегович', age: 23, gender: 'М', role: 'участник',depart: 2, is_deleted: false },
-        { id: 3, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович', age: 41, gender: 'М', role: 'участник',depart: 3, is_deleted: false },
-        { id: 4, first_name: 'SnowSnow', second_name: 'Jon', patronymic:'Олегович', age: 12, gender: 'М', role: 'участник',depart: 4, is_deleted: false },
-        { id: 5, first_name: 'Snow', second_name: 'Json', patronymic:'Олегович', age: 32, gender: 'М', role: 'участник',depart: 5, is_deleted: true },
-        { id: 6, first_name: 'Snow', second_name: 'Json', patronymic:'Олегович', age: 33, gender: 'М', role: 'участник',depart: 6, is_deleted: false },
-        { id: 7, first_name: 'Sanow', second_name: 'Json', patronymic:'Олегович', age: 44, gender: 'М', role: 'участник',depart: 7, is_deleted: false },
-        { id: 8, first_name: 'Snow', second_name: 'Josn', patronymic:'Олегович', age: 55, gender: 'М', role: 'участник',depart: 8, is_deleted: true },
-        { id: 9, first_name: 'Snaow', second_name: 'Jon', patronymic:'Олегович', age: 12, gender: 'М', role: 'участник',depart: 9, is_deleted: false },
-        { id: 10, first_name: 'SnowSnow', second_name: 'Joxn', patronymic:'Олегович', age: 123, gender: 'М', role: 'участник',depart: 10, is_deleted: false },
-        { id: 11, first_name: 'Snasow', second_name: 's', patronymic:'Олегович', age: 332, gender: 'М', role: 'участник',depart: 11, is_deleted: false },
-        { id: 12, first_name: 'Snsdaow', second_name: 'Jon', patronymic:'Олегович',age: 22, gender: 'М', role: 'участник',depart: 12, is_deleted: false },
-        { id: 13, first_name: 'Snosw', second_name: 'Jsson', patronymic:'Олегович',age: 55, gender: 'М', role: 'участник',depart: 13, is_deleted: false },
-        { id: 14, first_name: 'Sansaow', second_name: 'Jon', patronymic:'Олегович',age: 77, gender: 'М', role: 'участник',depart: 14, is_deleted: false },
-        { id: 15, first_name: 'Snsaow', second_name: 'Josan', patronymic:'Олегович',age: 6, gender: 'М', role: 'участник',depart: 15, is_deleted: false },
-        { id: 16, first_name: 'S1anow', second_name: 'Jon', patronymic:'Олегович',age: 3566, gender: 'М', role: 'участник',depart: 16, is_deleted: false },
-        { id: 17, first_name: 'Sn2ow', second_name: 'Jon', patronymic:'Олегович',age: 35, gender: 'М', role: 'участник',depart: 17, is_deleted: false },
-        { id: 18, first_name: 'Sndaow', second_name: 'Jon', patronymic:'Олегович',age: 345, gender: 'М', role: 'участник',depart: 18, is_deleted: false },
-        { id: 19, first_name: 'Sndsaow', second_name: 'Jon', patronymic:'Олегович',age: 345, gender: 'М', role: 'участник',depart: 19, is_deleted: false },
-        { id: 20, first_name: 'Snasow', second_name: 'Jon', patronymic:'Олегович',age: 3565, gender: 'М', role: 'участник',depart: 20, is_deleted: false },
-        { id: 21, first_name: 'Ssnow', second_name: 'Jon', patronymic:'Олегович',age: 3545, gender: 'М', role: 'участник',depart: 21, is_deleted: false },
-        { id: 22, first_name: 'Ssnow', second_name: 'Jon', patronymic:'Олегович',age: 456, gender: 'М', role: 'участник',depart: 22, is_deleted: true },
-        { id: 23, first_name: 'Snoaw', second_name: 'Jon', patronymic:'Олегович',age: 3555, gender: 'М', role: 'участник',depart: 23, is_deleted: false },
-        { id: 24, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович',age: 3545, gender: 'М', role: 'участник',depart: 24, is_deleted: false },
-        { id: 25, first_name: 'Sanow', second_name: 'Jon', patronymic:'Олегович',age: 354645, gender: 'М', role: 'участник',depart: 22, is_deleted: false },
-        { id: 26, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович',age: 359, gender: 'М', role: 'участник',depart: 26, is_deleted: false },
-        { id: 27, first_name: 'Snsow', second_name: 'Jon', patronymic:'Олегович',age: 305, gender: 'М', role: 'участник',depart: 23, is_deleted: false },
-        { id: 28, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович',age: 315, gender: 'М', role: 'участник',depart: 12, is_deleted: false },
-        { id: 29, first_name: 'Snow', second_name: 'Jon',patronymic:'Олегович', age: 325, gender: 'М', role: 'участник',depart: 21, is_deleted: false },
+        { id: 1, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович', age: 33, gender: 'М', role: 'участник',depart: 1, is_deleted: false, loading: false },
+        { id: 2, first_name: 'SnowSnow', second_name: 'Jon', patronymic:'Олегович', age: 23, gender: 'М', role: 'участник',depart: 2, is_deleted: false, loading: false },
+        { id: 3, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович', age: 41, gender: 'М', role: 'участник',depart: 3, is_deleted: false, loading: false },
+        { id: 4, first_name: 'SnowSnow', second_name: 'Jon', patronymic:'Олегович', age: 12, gender: 'М', role: 'участник',depart: 4, is_deleted: false, loading: false },
+        { id: 5, first_name: 'Snow', second_name: 'Json', patronymic:'Олегович', age: 32, gender: 'М', role: 'участник',depart: 5, is_deleted: true, loading: false },
+        { id: 6, first_name: 'Snow', second_name: 'Json', patronymic:'Олегович', age: 33, gender: 'М', role: 'участник',depart: 6, is_deleted: false, loading: false },
+        { id: 7, first_name: 'Sanow', second_name: 'Json', patronymic:'Олегович', age: 44, gender: 'М', role: 'участник',depart: 7, is_deleted: false, loading: false },
+        { id: 8, first_name: 'Snow', second_name: 'Josn', patronymic:'Олегович', age: 55, gender: 'М', role: 'участник',depart: 8, is_deleted: true, loading: false },
+        { id: 9, first_name: 'Snaow', second_name: 'Jon', patronymic:'Олегович', age: 12, gender: 'М', role: 'участник',depart: 9, is_deleted: false, loading: false },
+        { id: 10, first_name: 'SnowSnow', second_name: 'Joxn', patronymic:'Олегович', age: 123, gender: 'М', role: 'участник',depart: 10, is_deleted: false, loading: false },
+        { id: 11, first_name: 'Snasow', second_name: 's', patronymic:'Олегович', age: 332, gender: 'М', role: 'участник',depart: 11, is_deleted: false , loading: false},
+        { id: 12, first_name: 'Snsdaow', second_name: 'Jon', patronymic:'Олегович',age: 22, gender: 'М', role: 'участник',depart: 12, is_deleted: false, loading: false },
+        { id: 13, first_name: 'Snosw', second_name: 'Jsson', patronymic:'Олегович',age: 55, gender: 'М', role: 'участник',depart: 13, is_deleted: false, loading: false },
+        { id: 14, first_name: 'Sansaow', second_name: 'Jon', patronymic:'Олегович',age: 77, gender: 'М', role: 'участник',depart: 14, is_deleted: false, loading: false },
+        { id: 15, first_name: 'Snsaow', second_name: 'Josan', patronymic:'Олегович',age: 6, gender: 'М', role: 'участник',depart: 15, is_deleted: false, loading: false },
+        { id: 16, first_name: 'S1anow', second_name: 'Jon', patronymic:'Олегович',age: 3566, gender: 'М', role: 'участник',depart: 16, is_deleted: false, loading: false },
+        { id: 17, first_name: 'Sn2ow', second_name: 'Jon', patronymic:'Олегович',age: 35, gender: 'М', role: 'участник',depart: 17, is_deleted: false, loading: false },
+        { id: 18, first_name: 'Sndaow', second_name: 'Jon', patronymic:'Олегович',age: 345, gender: 'М', role: 'участник',depart: 18, is_deleted: false, loading: false },
+        { id: 19, first_name: 'Sndsaow', second_name: 'Jon', patronymic:'Олегович',age: 345, gender: 'М', role: 'участник',depart: 19, is_deleted: false, loading: false },
+        { id: 20, first_name: 'Snasow', second_name: 'Jon', patronymic:'Олегович',age: 3565, gender: 'М', role: 'участник',depart: 20, is_deleted: false, loading: false },
+        { id: 21, first_name: 'Ssnow', second_name: 'Jon', patronymic:'Олегович',age: 3545, gender: 'М', role: 'участник',depart: 21, is_deleted: false, loading: false },
+        { id: 22, first_name: 'Ssnow', second_name: 'Jon', patronymic:'Олегович',age: 456, gender: 'М', role: 'участник',depart: 22, is_deleted: true, loading: false },
+        { id: 23, first_name: 'Snoaw', second_name: 'Jon', patronymic:'Олегович',age: 3555, gender: 'М', role: 'участник',depart: 23, is_deleted: false, loading: false },
+        { id: 24, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович',age: 3545, gender: 'М', role: 'участник',depart: 24, is_deleted: false, loading: false },
+        { id: 25, first_name: 'Sanow', second_name: 'Jon', patronymic:'Олегович',age: 354645, gender: 'М', role: 'участник',depart: 22, is_deleted: false, loading: false },
+        { id: 26, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович',age: 359, gender: 'М', role: 'участник',depart: 26, is_deleted: false, loading: false },
+        { id: 27, first_name: 'Snsow', second_name: 'Jon', patronymic:'Олегович',age: 305, gender: 'М', role: 'участник',depart: 23, is_deleted: false, loading: false },
+        { id: 28, first_name: 'Snow', second_name: 'Jon', patronymic:'Олегович',age: 315, gender: 'М', role: 'участник',depart: 12, is_deleted: false, loading: false },
+        { id: 29, first_name: 'Snow', second_name: 'Jon',patronymic:'Олегович', age: 325, gender: 'М', role: 'участник',depart: 21, is_deleted: false, loading: false },
       ];
-      
-      
+    
+
     return (
 
         <div>
-            <EditDialog id={showEditDialog.id} visible={showEditDialog.open} close={setShowEditDialog}/> 
+            <EditDialog id={addState? '' : showEditDialog.id} visible={showEditDialog.open} close={setShowEditDialog} addState={addState}/> 
             <Button onClick={()=> {
                 setLoading(true);
                 setTimeout(() => {
@@ -128,19 +181,13 @@ export function Participants() {
 
             }}>Задать состояния</Button>
             <Button onClick={()=> {console.log(members)}}>Показать состояния</Button>
-            <TextField id="outlined-basic" label="Удалить" variant="outlined"  type='number' onInput={(event) => {removeFromParticipants({id: event.target.value});}}/>
-
+        
             <DataGrid 
                 apiRef={apiRef}
-                //stickyHeader
-                //slots={baseSelect}
                 loading={loading}
                 rows={members? members : ''}
                 columns={columns}
                 initialState={{
-                    // pagination: {
-                    //     paginationModel: { page: 0, pageSize: 5 },
-                    // },
                     
                 }}
                 columnVisibilityModel={columnVisibilityModel}
@@ -149,13 +196,19 @@ export function Participants() {
                 }
                 //autoPageSize={true}
                 autoHeight={true}
-                onRowClick={(params)=> { setShowEditDialog({open:true, id:params.row.id});  }}
+                onCellClick={(params, event)=> { 
+                    if(params.field !== 'action') {
+                        setAddState(false);
+                        setShowEditDialog({open:true, id:params.row.id}); 
+                    }
+                 }}
             />
 
             <Backdrop open={open} />
-            <SpeedDial
+            
+            {/* <SpeedDial
                 ariaLabel="SpeedDial tooltip example"
-                sx={{ position: 'fixed', bottom: 16, right: 16 }}
+                sx={{ position: 'fixed', bottom: 0, right: 16 }}
                 icon={<SpeedDialIcon />}
                 onClose={handleClose}
                 onOpen={handleOpen}
@@ -167,11 +220,16 @@ export function Participants() {
                         icon={action.icon}
                         tooltipTitle={action.name}
                         tooltipOpen
-                        onClick={handleClose}
+                        onClick={handleOpenAddDialog}
                     />
                 ))}
-            </SpeedDial>
-
+            </SpeedDial> */}
+            
+            <IconButton
+            style={{ position: 'fixed', bottom: 16, right: 16, backgroundColor: green[300], width: '60px', height: '60px' }}
+            onClick={handleOpenAddDialog}>
+                <AddIcon />
+            </IconButton>
         </div>
     );
 }
