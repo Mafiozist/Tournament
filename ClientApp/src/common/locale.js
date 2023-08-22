aa4d4b1ce792fc76c9ec58e7b66d15818823db1a
dinar <chaneldinara@gmail.com>
2023-08-22 23:23:49 +0700

    Вынес сообщения и метки в отдельный файл

---
+++ ClientApp/src/common/locale.js
@@ -0,0 +1,72 @@
+
+export const muiDataGridLocale = {
+    columnMenuSortAsc:"Убыв",
+    columnMenuSortDesc:"Возр",
+    filterOperatorAfter:"После",
+    filterOperatorBefore:'До',
+    filterOperatorContains:'Содержит',
+    filterOperatorEndsWith:'Заканчивается на',
+    filterOperatorEquals:'Равно',
+    filterOperatorIs:'Равно',
+    filterOperatorIsAnyOf:'Любой из',
+    filterOperatorIsEmpty: 'Пустая',
+    filterOperatorIsNotEmpty:'Не пустая',
+    filterOperatorNot:'Не',
+    filterOperatorOnOrAfter:'Равно или после',
+    filterOperatorOnOrBefore:'Равно или до',
+    filterOperatorStartsWith:'Начинается с',
+    filterPanelInputPlaceholder:'Ввод...',
+    filterValueAny:'Любой',
+    columnMenuUnsort:'Убрать сортировку',
+    columnMenuHideColumn:'Скрыть',
+    columnMenuManageColumns: 'Управлять колонками',
+    columnMenuShowColumns:'Показать колонки',
+    columnsPanelShowAllButton: 'Показать все',
+    columnsPanelHideAllButton: 'Скрыть все',
+    columnMenuFilter:'Фильтр',
+    columnsMenuLabel:'Колонка',
+    noRowsLabel:'Данных для отображения не обнаружено', 
+    footerRowSelected: (num)=> <div style={{width:'100px'}}>{`Выбрана(-о) ${num} строк`}</div>
+};
+
+export const mainPageHeaderLocale = "Главная";
+export const participantPageHeaderLocale = "Участники";
+export const teamsPageHeaderLocale = "Команды";
+export const tournamentPageHeaderLocale = "Турниры";
+
+export const deleteLocale = "Удалить";
+export const saveLocale = "Сохранить";
+export const cancelLocale = "Отменить";
+export const fioLocale = "ФИО";
+export const lastNameLocale = "Фамилия";
+export const patronymicLocale = "Отчество";
+export const firstNameLocale = "Имя";
+export const teamLabelLocale ='Название команды';
+export const addMemToTeamLocale = "Добавить участников в команду";
+export const dataNotFoundLocale = "Данных для отображения не было обнаружено";
+export const yearsOldLocale = "Возраст";
+export const genderLocale = "Пол";
+export const roleLocale = "Роль";
+export const departLocale = 'Отдел/Цех';
+export const deletedLocale = 'Удален?';
+export const changeLocale = "Изменить";
+export const hasInTeamsLocale = "Состоит в командах";
+export const startToInputLocale = "Введите";
+export const applyChoiceLocale = "Подтвердить выбор";
+export const defaultSearchLocale = "Начните набирать...";
+export const showDeletedLocale = "Показать с удаленными";
+export const showTeamMembersHistoryLocale = "Показать историю изменения командного состава";
+export const dateTimeLabelLocale = "Дата/время";
+export const defaultSuccessDeleteLocale = "Успешно удалено";
+export const returnDataLocale = "Восстановить";
+
+//Сообщения об ошибках
+export const defaultErrorMessage = "Возникла ошибка при обработке запроса."
+export const teamsMembersGetErrorMessage = "Не удалось получить участников команды";
+export const teamsMembersHistoryGetErrorMessage = "Не удалось получить историю";
+export const teamSaveErrorMessage = "Не удалось сохранить информацию о команде";
+export const noDataErrorLocale = "Нет данных для отображения";
+
+
+
+
