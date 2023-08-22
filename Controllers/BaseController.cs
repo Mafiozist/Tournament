27934a9d85018c19b1cb1b0b392843e8d42f549e
dinar <chaneldinara@gmail.com>
2023-08-22 23:58:43 +0700

    Добавил базовый контроллер для реализации общих методов, пока там передается конфигурация для доступа к бд

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Tournament.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected IConfiguration _configuration;
+        protected string _connectionString;
+
+        public BaseController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            _connectionString = _configuration.GetConnectionString("competition");
+        }
+    }
+}
